d69217b7b176933c18a880133fe28b3c92c95de0
Daniel Macauyag <dmacauyag92@gmail.com>
2017-05-14 16:27:36 -0700

    connected to mongo db

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const
   bodyParser = require('body-parser'),
   port = 3000
 
+mongoose.connect('mongodb://localhost/stalitics', (err) => {
+  console.log(err || "Connected to MongoDB (stalitics)")
+})
+
 app.listen(port, (err) => {
   console.log(err || "Server listening on port:", port)
 })
