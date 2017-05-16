7ebb0cc5ce6685d7202acf99e09558b792b2ccfd
Jordan Hall <jordanhall.1995@yahoo.com>
2017-05-16 12:26:15 -0700

    made new routes

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const
   express = require('express'),
   passport = require('passport'),
-  userRouter = express.Router()
+  userRouter = express.Router(),
+  User = require('../models/User.js')
 
 userRouter.route('/login')
   .get((req, res) => {
@@ -14,7 +15,7 @@ userRouter.route('/login')
 
 userRouter.route('/signup')
   .get((req, res) => {
-    res.render('signup', {message: req.flash('signupMessage')})
+    res.render('pages/signup', {message: req.flash('signupMessage')})
   })
   .post(passport.authenticate('local-signup', {
     successRedirect: '/profile',
@@ -26,6 +27,13 @@ userRouter.get('/profile', isLoggedIn, (req, res) => {
   res.render('pages/profile', {user: req.user})
 })
 
+userRouter.get('/users/:id', (req, res) => {
+  User.findById(req.params.id, (err, user) => {
+    console.log(user)
+    res.render('pages/index', {user: user})
+  })
+})
+
 userRouter.get('/logout', isLoggedIn, (req, res) => {
   req.logout()
   res.redirect('/')
@@ -33,6 +41,18 @@ userRouter.get('/logout', isLoggedIn, (req, res) => {
 
 userRouter.get('/favorites', isLoggedIn, (req, res) => {
   res.render('pages/favorites')
+})
+
+userRouter.get('/featured', (req, res) => {
+  res.render('pages/featured')
+})
+
+userRouter.get('/tweets', (req, res) => {
+  res.render('pages/tweets')
+})
+
+userRouter.get('/hashtags', (req, res) => {
+  res.render('pages/hashtags')
 })
 
 function isLoggedIn(req, res, next) {
