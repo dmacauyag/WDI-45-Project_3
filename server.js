675e9065cea3653b959ac4234347c3b1bc4bf478
Daniel Macauyag <dmacauyag92@gmail.com>
2017-05-16 09:59:13 -0700

    added twitter keys to .env

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ const
   userRoutes = require('./routes/users.js')
 
 const twitterClient = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
+  consumer_key: 'process.env.TWITTER_CONSUMER_KEY',
+  consumer_secret: 'process.env.TWITTER_CONSUMER_SECRET',
+  access_token_key: 'process.env.TWITTER_ACCESS_TOKEN',
+  access_token_secret: 'process.env.TWITTER_ACCESS_TOKEN_SECRET'
 })
 
 // environment port
